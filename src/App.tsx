e946bdbd8ec64689b9299a6b8ed06f4fa086b4b3
josefaidt <josef.aidt@gmail.com>
2024-10-07 10:35:47 -0700

    changes to resolve state issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Authenticator } from "@aws-amplify/ui-react"
-import "@aws-amplify/ui-react/styles.css"
+import { useAuthenticator } from "@aws-amplify/ui-react"
 import { useEffect, useState } from "react"
 import type { Schema } from "../amplify/data/resource"
 import { generateClient } from "aws-amplify/data"
@@ -7,6 +6,7 @@ import { generateClient } from "aws-amplify/data"
 const client = generateClient<Schema>()
 
 function App() {
+  const { signOut } = useAuthenticator()
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([])
 
   useEffect(() => {
@@ -24,29 +24,25 @@ function App() {
   }
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <main>
-          <h1>My todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-                {todo.content}
-              </li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.content}
+          </li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+      <button onClick={signOut}>Sign out</button>
+    </main>
   )
 }
 
